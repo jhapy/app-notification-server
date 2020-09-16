872b546a759944dcce4600655b5248cf030e9d85
jHapy Lead Dev <jhapy@jhapy.org>
2020-09-16 11:58:16 +0100

    Fixes

--- src/main/java/org/jhapy/notification/config/JmsConfig.java
+++ src/main/java/org/jhapy/notification/config/JmsConfig.java
@@ -1,7 +1,5 @@
 package org.jhapy.notification.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
